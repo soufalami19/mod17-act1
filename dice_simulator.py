fa0d2c9e857103222aaad39b756328a585169506
Soufiane Alami <souf@Air-de-Soufiane.lan>
2024-11-01 13:04:59 +0100

    Modificar para que realice 6 lanzamientos de un dado de 6 caras en main

--- dice_simulator.py
+++ dice_simulator.py
@@ -4,6 +4,6 @@ from time import sleep
 def roll(amount: int, sides: int):
     return dice.roll(f'{amount}d{sides}')
 
-for idx, result in enumerate(roll(5, 6)):
+for idx, result in enumerate(roll(6, 6)):
     print(f'Lanzamiento {idx+1} número obtenido {result}')
     sleep(5)
